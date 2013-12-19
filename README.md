6d9378c8861c96010fa3d80bbfdc1eb328053612
Tony Gutierrez <pursual@gmail.com>
2013-12-19 18:40:34 -0500

    Initial commit.

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
-RaviTDC
-=======
+# Turner Developer Challenge
+Thanks for considering me (Tony Gutierrez) for a job at Turner Broadcasting.
 
-Turner Development Challenge
+
+## Setup
+
+You should be able to npm install with the included /tweets/package.json. Then run "node app.js" in the "tweets" folder. The app runs on port 3000.
+
+## Features
+
+* Put any twitter handle in the url (i.e http://localhost:3000/cnn)
+* Using DustJS and a custom date helper on the client side
+* Urls are clickable.
+* Tweets are clickable.
+* rudimentary image displaying.
+
+
+## ToDos
+In a production environment I would need error handling for the ajax request. Also all node routes need error handling.
+
+The node api could return less data per tweet as I am not using all fields in the UI.
+
---
+++ tweets/app.js
@@ -0,0 +1,63 @@
+/**
+ * Module dependencies.
+ */
+var express = require('express');
+var Twitter = require('mtwitter');
+var hbs = require('hbs');
+
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
+app.engine('html', hbs.__express);
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+
+
+var t = new Twitter({
+  consumer_key: 'vEAmcUdgeSpdGiMbH1FMZw',
+  consumer_secret: 'GC8j8FBIRDxiKgUwtB0idqDAmADTAF0SW7Yc7OUA',
+  application_only: true
+});
+
+
+app.get('/:id', function (req, res) {
+  var screen_name = (req.params.id === 'cnnbrk-tweets') ? 'cnnbrk' : req.params.id;
+  res.render('index', {
+    screen_name: screen_name
+  });
+});
+
+
+app.get('/api/tweets', function (req, res) {
+  var screen_name = req.query.screen_name;
+
+  t.get("statuses/user_timeline", {
+    screen_name: screen_name,
+    count: 10
+  },
+    function (err, feed) {
+      if (err) {
+        throw err;
+      }
+      res.send(feed);
+    });
+});
+
+
+http.createServer(app).listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
+});
